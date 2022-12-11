85dfff293f50737cf24fa377e74d21b29118008b
limelollipop <119836167+limelollipop@users.noreply.github.com>
2022-12-11 17:28:59 +0900

    event 슬라이드 js 실패

---
+++ js/main.js
@@ -0,0 +1,61 @@
+const eventSlide = document.querySelector("#event");
+const slideWrap = eventSlide.querySelector("div .wrap")
+const lis = slideWrap.querySelectorAll(".right .wrap article");
+const prev = eventSlide.querySelector(".left .btnPrev");
+const next = eventSlide.querySelector(".left .btnNext");
+
+
+// let len = lis.length;
+// console.log(slideWrap);
+// let speed = 500;
+
+// init();
+
+// prev.addEventListener("click", (e)=>{
+//     e.preventDefault();
+
+//     prevSlide();
+// });
+
+// next.addEventListener("click", (e)=>{
+//     e.preventDefault();
+
+//     nextSlide();
+// });
+
+// function init(){
+//     slideWrap.style.left = "-25%"
+//     slideWrap.prepend(slideWrap.lastElementChild);
+
+//     // slideWrap.style.width = `${100 * len}%`;
+
+//     // lis. forEach((el)=>{
+//     //     el.style.width = `%{100 * / len}`;
+//     // });
+// }
+
+// function nextSlide(){
+//     new Anim(slideWrap,{
+//         prop : 'left',
+//         value : '-100%',
+//         duration : speed,
+
+//         callback : ()=>{
+//             slideWrap.style.left = "-25%";
+//             slideWrap.append(slideWrap.firstElementChild);
+//         }
+//     });
+// }
+
+// function prevSlide(){
+//     new Anim(slideWrap,{
+//         prop : 'left',
+//         value : '0%',
+//         duration : speed,
+
+//         callback : ()=>{
+//             slideWrap.style.left = "-25%";
+//             slideWrap.append(slideWrap.lastElementChild);
+//         }
+//     });
+// }
