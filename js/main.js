56b6970ab5908e4f6dbb0c757181ffdf4a761d6d
limelollipop <119836167+limelollipop@users.noreply.github.com>
2022-12-20 08:57:47 +0900

    서브페이지 html 구현

--- js/main.js
+++ js/main.js
@@ -1,27 +1,17 @@
+// menuMo script
 const btnMenu = document.querySelector(".btnMenu");
 const menuMo = document.querySelector(".menuMo");
 
+// menuMo script
 btnMenu.addEventListener("click",(e)=>{
     e.preventDefault();
     btnMenu.classList.toggle("on");
     menuMo.classList.toggle("on");
 });
 
-const slider = document.querySelector("#visual");
-const panel = slider.querySelector(".panel");
-const imgLi = slider.querySelectorAll("img");
-const btn = slider.querySelector(".btn");
-// console.log(panel);
+// const slider = document.querySelector("#visual");
+// const panel = slider.querySelector(".panel");
+// const imgLis = slider.querySelectorAll("img");
+// const btn = slider.querySelector(".btn");
+// // console.log(panel);
 
-btn.forEach((el, index)=>{
-    el.addEventListener("click", (e)=>{
-        e.preventDefault();
-
-        new Animation(panel,{
-            prop: "margin-left",
-            value: -100 * index + "%",
-            duration: 1000,
-        });
-    
-    })    
-});
