976b6df222c0726051f8194ef2c8d27595ef7efe
limelollipop <yesseobaek@gmail.com>
2022-12-31 00:30:15 +0900

    subpage things to do load 효과 스크립트 추가(기본)

--- js/things.js
+++ js/things.js
@@ -1,7 +1,13 @@
+const thingsFrame = document.querySelector('main .isoWrap');
+const thingsImgs = thingsFrame.querySelectorAll('article');
+// console.log(thingsImgs);
+
 window.addEventListener('load', () => {
 	const grid = new Isotope('.isoWrap', {
 		itemSelection: 'article',
 		columnWidth: 'article',
 		transitionDuration: '0.5s',
 	});
+
+	thingsFrame.classList.add('on');
 });
