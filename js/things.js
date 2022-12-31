6a8851a4e65a3f761eac11c9cde35d647b7248a1
limelollipop <yesseobaek@gmail.com>
2023-01-01 01:17:22 +0900

    subpage visual 영역 css 수정

--- js/things.js
+++ js/things.js
@@ -1,13 +1,51 @@
 const thingsFrame = document.querySelector('main .isoWrap');
 const thingsImgs = thingsFrame.querySelectorAll('article');
-// console.log(thingsImgs);
+
+const loading = document.querySelector('.loading');
+
+// console.log(Array.from(thingsFrame.querySelectorAll('article')));
 
 window.addEventListener('load', () => {
-	const grid = new Isotope('.isoWrap', {
+	loading.classList.add('off');
+	thingsFrame.classList.add('on');
+
+	new Isotope('.isoWrap', {
 		itemSelection: 'article',
 		columnWidth: 'article',
 		transitionDuration: '0.5s',
 	});
-
-	thingsFrame.classList.add('on');
 });
+
+/* 시도 해봤던 사진 로딩 후 이미지 등장
+const thingsFrame = document.querySelector('main .isoWrap');
+const thingsImgs = thingsFrame.querySelectorAll('article');
+
+const loading = document.querySelector('.loading');
+
+// console.log(Array.from(thingsFrame.querySelectorAll('article')));
+
+// window.addEventListener('load', () => {
+const len = thingsImgs.length;
+
+let count = 0;
+
+for (let el of thingsImgs) {
+	el.addEventListener('load', () => {
+		count++;
+		if (count == len) isoLayout();
+	});
+}
+
+function isoLayout() {
+	loading.classList.add('off');
+	thingsFrame.classList.add('on');
+
+	new Isotope('.isoWrap', {
+		itemSelection: 'article',
+		columnWidth: 'article',
+		transitionDuration: '0.5s',
+	});
+}
+
+// });
+*/
