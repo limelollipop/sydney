1c1ed5a17fbfa91b0c4b99467093787fdd4c6df0
limelollipop <yesseobaek@gmail.com>
2023-01-13 17:02:34 +0900

    main 마지막 deal영역 스크롤 모션 오류 수정(나누기 3 혹은 고정값)

--- js/scroll.js
+++ js/scroll.js
@@ -3,6 +3,7 @@ const scrollBoxs = document.querySelectorAll('figure, body > section');
 const scrollBtns = document.querySelectorAll('body > ul li');
 const whatsOn = document.querySelector('body #visual .inner .btn');
 const speed = 500;
+// 나누기 3이나 고정값
 const base = -window.innerHeight / 3;
 let posArr = [];
 const scrollBtnsArr = Array.from(scrollBtns);
